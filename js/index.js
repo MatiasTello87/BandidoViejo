36c07edbfa58a42ce87d2650aa5f81bccf15a216
Matias Tello <matiasfernandotello@gmail.com>
2024-11-11 02:14:05 -0300

    navbar

---
+++ js/index.js
@@ -0,0 +1,19 @@
+let nav =document.querySelector('.nav_links');
+let hamburg= document.querySelector('.hamburg')
+let cerrar= document.querySelector('.cerrar')
+
+
+hamburg.addEventListener('click',()=> {
+    hamburg.style.display= 'none';
+    nav.style.display= 'block';
+    cerrar.style.display= 'block';  
+});
+
+// Boton cerrar del nav
+cerrar.addEventListener('click',()=>{
+    hamburg.style.display= 'block';
+    nav.style.display= 'none';
+    cerrar.style.display= 'none';  
+
+
+})
